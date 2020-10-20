4bb7e71c7e78384dd80b601289afd08e5167491c
gabrielifeliz <gabrielifeliz@users.noreply.github.com>
2020-10-20 17:48:40 -0400

    changes to labs and assignments

---
+++ Lab_5/js/scripts.js
@@ -0,0 +1,360 @@
+let myFullName = "Isaac Feliz"; // My full name
+document.querySelector("body").appendChild(document.createElement("header")); // Retrieve and add a header to web page's body
+document
+  .querySelector("header") // Retrieve web page's header
+  .appendChild(document.createTextNode(myFullName + "'s List Of Countries")); // Include text to web page's header
+let myOL = document.createElement("ol"); // Create ordered list
+myOL.className = "countries"; // Include class name 'countries' to empty ordered list
+document.querySelector("body").appendChild(myOL); // Retrieve and add empty ordered list to the web page's body after header
+let myBtn = document.createElement("button"); // Create button
+myBtn.textContent = "Select 25 Random Countries"; // Add text to button
+document.querySelector("body").appendChild(myBtn); // Retrieve and add button to the web page's body after empty ordered list
+
+/* 
+    Function that populates ordered list with 25 random countries in an alphabetic order
+*/
+function selectCountries() {
+  // All countries with name and code listed from IBAN
+  const allCountries = [
+    { name: "Afghanistan", code: "AF" },
+    { name: "Åland Islands", code: "AX" },
+    { name: "Albania", code: "AL" },
+    { name: "Algeria", code: "DZ" },
+    { name: "American Samoa", code: "AS" },
+    { name: "Andorra", code: "AD" },
+    { name: "Angola", code: "AO" },
+    { name: "Anguilla", code: "AI" },
+    { name: "Antarctica", code: "AQ" },
+    { name: "Antigua and Barbuda", code: "AG" },
+    { name: "Argentina", code: "AR" },
+    { name: "Armenia", code: "AM" },
+    { name: "Aruba", code: "AW" },
+    { name: "Australia", code: "AU" },
+    { name: "Austria", code: "AT" },
+    { name: "Azerbaijan", code: "AZ" },
+    { name: "Bahamas", code: "BS" },
+    { name: "Bahrain", code: "BH" },
+    { name: "Bangladesh", code: "BD" },
+    { name: "Barbados", code: "BB" },
+    { name: "Belarus", code: "BY" },
+    { name: "Belgium", code: "BE" },
+    { name: "Belize", code: "BZ" },
+    { name: "Benin", code: "BJ" },
+    { name: "Bermuda", code: "BM" },
+    { name: "Bhutan", code: "BT" },
+    { name: "Bolivia", code: "BO" },
+    { name: "Bosnia and Herzegovina", code: "BA" },
+    { name: "Botswana", code: "BW" },
+    { name: "Bouvet Island", code: "BV" },
+    { name: "Brazil", code: "BR" },
+    { name: "British Indian Ocean Territory", code: "IO" },
+    { name: "Brunei Darussalam", code: "BN" },
+    { name: "Bulgaria", code: "BG" },
+    { name: "Burkina Faso", code: "BF" },
+    { name: "Burundi", code: "BI" },
+    { name: "Cambodia", code: "KH" },
+    { name: "Cameroon", code: "CM" },
+    { name: "Canada", code: "CA" },
+    { name: "Cape Verde", code: "CV" },
+    { name: "Cayman Islands", code: "KY" },
+    { name: "Central African Republic", code: "CF" },
+    { name: "Chad", code: "TD" },
+    { name: "Chile", code: "CL" },
+    { name: "China", code: "CN" },
+    { name: "Christmas Island", code: "CX" },
+    { name: "Cocos (Keeling) Islands", code: "CC" },
+    { name: "Colombia", code: "CO" },
+    { name: "Comoros", code: "KM" },
+    { name: "Congo", code: "CG" },
+    { name: "Congo, The Democratic Republic of the", code: "CD" },
+    { name: "Cook Islands", code: "CK" },
+    { name: "Costa Rica", code: "CR" },
+    { name: "Cote D`Ivoire", code: "CI" },
+    { name: "Croatia", code: "HR" },
+    { name: "Cuba", code: "CU" },
+    { name: "Cyprus", code: "CY" },
+    { name: "Czech Republic", code: "CZ" },
+    { name: "Denmark", code: "DK" },
+    { name: "Djibouti", code: "DJ" },
+    { name: "Dominica", code: "DM" },
+    { name: "Dominican Republic", code: "DO" },
+    { name: "Ecuador", code: "EC" },
+    { name: "Egypt", code: "EG" },
+    { name: "El Salvador", code: "SV" },
+    { name: "Equatorial Guinea", code: "GQ" },
+    { name: "Eritrea", code: "ER" },
+    { name: "Estonia", code: "EE" },
+    { name: "Ethiopia", code: "ET" },
+    { name: "Falkland Islands (Malvinas)", code: "FK" },
+    { name: "Faroe Islands", code: "FO" },
+    { name: "Fiji", code: "FJ" },
+    { name: "Finland", code: "FI" },
+    { name: "France", code: "FR" },
+    { name: "French Guiana", code: "GF" },
+    { name: "French Polynesia", code: "PF" },
+    { name: "French Southern Territories", code: "TF" },
+    { name: "Gabon", code: "GA" },
+    { name: "Gambia", code: "GM" },
+    { name: "Georgia", code: "GE" },
+    { name: "Germany", code: "DE" },
+    { name: "Ghana", code: "GH" },
+    { name: "Gibraltar", code: "GI" },
+    { name: "Greece", code: "GR" },
+    { name: "Greenland", code: "GL" },
+    { name: "Grenada", code: "GD" },
+    { name: "Guadeloupe", code: "GP" },
+    { name: "Guam", code: "GU" },
+    { name: "Guatemala", code: "GT" },
+    { name: "Guernsey", code: "GG" },
+    { name: "Guinea", code: "GN" },
+    { name: "Guinea-Bissau", code: "GW" },
+    { name: "Guyana", code: "GY" },
+    { name: "Haiti", code: "HT" },
+    { name: "Heard Island and Mcdonald Islands", code: "HM" },
+    { name: "Holy See (Vatican City State)", code: "VA" },
+    { name: "Honduras", code: "HN" },
+    { name: "Hong Kong", code: "HK" },
+    { name: "Hungary", code: "HU" },
+    { name: "Iceland", code: "IS" },
+    { name: "India", code: "IN" },
+    { name: "Indonesia", code: "ID" },
+    { name: "Iran, Islamic Republic Of", code: "IR" },
+    { name: "Iraq", code: "IQ" },
+    { name: "Ireland", code: "IE" },
+    { name: "Isle of Man", code: "IM" },
+    { name: "Israel", code: "IL" },
+    { name: "Italy", code: "IT" },
+    { name: "Jamaica", code: "JM" },
+    { name: "Japan", code: "JP" },
+    { name: "Jersey", code: "JE" },
+    { name: "Jordan", code: "JO" },
+    { name: "Kazakhstan", code: "KZ" },
+    { name: "Kenya", code: "KE" },
+    { name: "Kiribati", code: "KI" },
+    { name: "Korea, Democratic People'S Republic of", code: "KP" },
+    { name: "Korea, Republic of", code: "KR" },
+    { name: "Kuwait", code: "KW" },
+    { name: "Kyrgyzstan", code: "KG" },
+    { name: "Lao People'S Democratic Republic", code: "LA" },
+    { name: "Latvia", code: "LV" },
+    { name: "Lebanon", code: "LB" },
+    { name: "Lesotho", code: "LS" },
+    { name: "Liberia", code: "LR" },
+    { name: "Libyan Arab Jamahiriya", code: "LY" },
+    { name: "Liechtenstein", code: "LI" },
+    { name: "Lithuania", code: "LT" },
+    { name: "Luxembourg", code: "LU" },
+    { name: "Macao", code: "MO" },
+    { name: "Macedonia, The Former Yugoslav Republic of", code: "MK" },
+    { name: "Madagascar", code: "MG" },
+    { name: "Malawi", code: "MW" },
+    { name: "Malaysia", code: "MY" },
+    { name: "Maldives", code: "MV" },
+    { name: "Mali", code: "ML" },
+    { name: "Malta", code: "MT" },
+    { name: "Marshall Islands", code: "MH" },
+    { name: "Martinique", code: "MQ" },
+    { name: "Mauritania", code: "MR" },
+    { name: "Mauritius", code: "MU" },
+    { name: "Mayotte", code: "YT" },
+    { name: "Mexico", code: "MX" },
+    { name: "Micronesia, Federated States of", code: "FM" },
+    { name: "Moldova, Republic of", code: "MD" },
+    { name: "Monaco", code: "MC" },
+    { name: "Mongolia", code: "MN" },
+    { name: "Montserrat", code: "MS" },
+    { name: "Morocco", code: "MA" },
+    { name: "Mozambique", code: "MZ" },
+    { name: "Myanmar", code: "MM" },
+    { name: "Namibia", code: "NA" },
+    { name: "Nauru", code: "NR" },
+    { name: "Nepal", code: "NP" },
+    { name: "Netherlands", code: "NL" },
+    { name: "Netherlands Antilles", code: "AN" },
+    { name: "New Caledonia", code: "NC" },
+    { name: "New Zealand", code: "NZ" },
+    { name: "Nicaragua", code: "NI" },
+    { name: "Niger", code: "NE" },
+    { name: "Nigeria", code: "NG" },
+    { name: "Niue", code: "NU" },
+    { name: "Norfolk Island", code: "NF" },
+    { name: "Northern Mariana Islands", code: "MP" },
+    { name: "Norway", code: "NO" },
+    { name: "Oman", code: "OM" },
+    { name: "Pakistan", code: "PK" },
+    { name: "Palau", code: "PW" },
+    { name: "Palestinian Territory, Occupied", code: "PS" },
+    { name: "Panama", code: "PA" },
+    { name: "Papua New Guinea", code: "PG" },
+    { name: "Paraguay", code: "PY" },
+    { name: "Peru", code: "PE" },
+    { name: "Philippines", code: "PH" },
+    { name: "Pitcairn", code: "PN" },
+    { name: "Poland", code: "PL" },
+    { name: "Portugal", code: "PT" },
+    { name: "Puerto Rico", code: "PR" },
+    { name: "Qatar", code: "QA" },
+    { name: "Reunion", code: "RE" },
+    { name: "Romania", code: "RO" },
+    { name: "Russian Federation", code: "RU" },
+    { name: "RWANDA", code: "RW" },
+    { name: "Saint Helena", code: "SH" },
+    { name: "Saint Kitts and Nevis", code: "KN" },
+    { name: "Saint Lucia", code: "LC" },
+    { name: "Saint Pierre and Miquelon", code: "PM" },
+    { name: "Saint Vincent and the Grenadines", code: "VC" },
+    { name: "Samoa", code: "WS" },
+    { name: "San Marino", code: "SM" },
+    { name: "Sao Tome and Principe", code: "ST" },
+    { name: "Saudi Arabia", code: "SA" },
+    { name: "Senegal", code: "SN" },
+    { name: "Serbia and Montenegro", code: "CS" },
+    { name: "Seychelles", code: "SC" },
+    { name: "Sierra Leone", code: "SL" },
+    { name: "Singapore", code: "SG" },
+    { name: "Slovakia", code: "SK" },
+    { name: "Slovenia", code: "SI" },
+    { name: "Solomon Islands", code: "SB" },
+    { name: "Somalia", code: "SO" },
+    { name: "South Africa", code: "ZA" },
+    { name: "South Georgia and the South Sandwich Islands", code: "GS" },
+    { name: "Spain", code: "ES" },
+    { name: "Sri Lanka", code: "LK" },
+    { name: "Sudan", code: "SD" },
+    { name: "Suriname", code: "SR" },
+    { name: "Svalbard and Jan Mayen", code: "SJ" },
+    { name: "Swaziland", code: "SZ" },
+    { name: "Sweden", code: "SE" },
+    { name: "Switzerland", code: "CH" },
+    { name: "Syrian Arab Republic", code: "SY" },
+    { name: "Taiwan, Province of China", code: "TW" },
+    { name: "Tajikistan", code: "TJ" },
+    { name: "Tanzania, United Republic of", code: "TZ" },
+    { name: "Thailand", code: "TH" },
+    { name: "Timor-Leste", code: "TL" },
+    { name: "Togo", code: "TG" },
+    { name: "Tokelau", code: "TK" },
+    { name: "Tonga", code: "TO" },
+    { name: "Trinidad and Tobago", code: "TT" },
+    { name: "Tunisia", code: "TN" },
+    { name: "Turkey", code: "TR" },
+    { name: "Turkmenistan", code: "TM" },
+    { name: "Turks and Caicos Islands", code: "TC" },
+    { name: "Tuvalu", code: "TV" },
+    { name: "Uganda", code: "UG" },
+    { name: "Ukraine", code: "UA" },
+    { name: "United Arab Emirates", code: "AE" },
+    { name: "United Kingdom", code: "GB" },
+    { name: "United States", code: "US" },
+    { name: "United States Minor Outlying Islands", code: "UM" },
+    { name: "Uruguay", code: "UY" },
+    { name: "Uzbekistan", code: "UZ" },
+    { name: "Vanuatu", code: "VU" },
+    { name: "Venezuela", code: "VE" },
+    { name: "Viet Nam", code: "VN" },
+    { name: "Virgin Islands, British", code: "VG" },
+    { name: "Virgin Islands, U.S.", code: "VI" },
+    { name: "Wallis and Futuna", code: "WF" },
+    { name: "Western Sahara", code: "EH" },
+    { name: "Yemen", code: "YE" },
+    { name: "Zambia", code: "ZM" },
+    { name: "Zimbabwe", code: "ZW" },
+  ];
+
+  // Infinite loop, adding countries to list one by one, until the ordered list reaches maximum capacity
+  while (true) {
+    // Randomly choosen country
+    let choosenCountry =
+      allCountries[Math.floor(Math.random() * Math.floor(allCountries.length))];
+    // Create a list item
+    let myLI = document.createElement("li");
+    // Include text to list item
+    myLI.innerHTML =
+      "<strong>" + choosenCountry["code"] + "</strong> " + choosenCountry["name"];
+    // Retrieve ordered list
+    let myCountryList = document.querySelector("ol");
+    // Create boolean variable to check whether the country exists in the list
+    let existingItem = false;
+    // Retrieve existing list items if any
+    let countriesLI = document.querySelectorAll("li");
+    if (countriesLI.length > 0) {
+      if (countriesLI.length != 25) {
+        /* If the ordered list is not empty, but is not of length 25,
+           check whether or not the randomly selected country is in the list */
+        countriesLI.forEach((country) => {
+          if (country.textContent.split(" ")[0] === choosenCountry["code"]) {
+            existingItem = true; // If randomly selected country is in the list, mark the variable as true
+          }
+        });
+        if (!existingItem) {
+          myCountryList.appendChild(myLI); // If randomly selected country is not in the list, add it
+        }
+      } else {
+        sortList(); // Sort list in alphabetical order
+        logUnselectedCountries(allCountries); // Log unselected countries to console
+        break;
+      }
+    } else {
+      myCountryList.appendChild(myLI); // If list is empty, add country name and code
+    }
+  }
+}
+
+/* Function to sort country list in alphabetical order
+   Credit: https://www.w3schools.com/howto/howto_js_sort_list.asp
+   I made changes for the function to work here
+*/
+function sortList() {
+  let i, switching, b, shouldSwitch;
+  switching = true;
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // start by saying: no switching is done:
+    switching = false;
+    b = document.querySelector("ol").getElementsByTagName("li");
+    // Loop through all list-items:
+    for (i = 0; i < (b.length - 1); i++) {
+      // start by saying there should be no switching:
+      shouldSwitch = false;
+      /* check if the next item should
+      switch place with the current item: */
+      if (b[i].innerHTML.split(" ")[1].toLowerCase() > b[i + 1].innerHTML.split(" ")[1].toLowerCase()) {
+        /* if next item is alphabetically
+        lower than current item, mark as a switch
+        and break the loop: */
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark the switch as done: */
+      b[i].parentNode.insertBefore(b[i + 1], b[i]);
+      switching = true;
+    }
+  }
+}
+
+/* 
+    Function to log unselected countries to console
+*/
+function logUnselectedCountries(countries) {
+  // Create an array for selected countries
+  let selectedCountries = new Array();
+  // Retrieve list items from HTML file
+  let b = document.querySelector("ol").getElementsByTagName("li");
+  for (let i = 0; i < (b.length - 1); i++) {
+    // Add selected countries to the array
+    selectedCountries.push(b[i].innerText);
+  }
+  for (let i = 0; i < (countries.length - 1); i++) {
+    if (!selectedCountries.includes(countries[i]["code"])) {
+      // Log countries that aren't included in the selected countries list
+      console.log(countries[i]["code"] + " " + countries[i]["name"]);
+    }
+  }   
+}
+
+// Add event listener
+document.querySelector("button").addEventListener("click", selectCountries);
