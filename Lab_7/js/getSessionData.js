d24921fdbed7b2072fde3efd9a3ac01dea678177
gabrielifeliz <gabrielifeliz@users.noreply.github.com>
2020-10-29 19:50:01 -0400

    lab 7 fix

--- Lab_7/js/getSessionData.js
+++ Lab_7/js/getSessionData.js
@@ -1,8 +1,8 @@
- // Get saved data from sessionStorage
+// Get saved data from sessionStorage
 let name = sessionStorage.getItem("name"); // Your code here
 let semester = sessionStorage.getItem("semester"); // Your code here
 let credits = sessionStorage.getItem("credits"); // Your code here
-let description = sessionStorage.getItem("description"); // Your code here 
+let description = sessionStorage.getItem("description"); // Your code here
 
 // Your code here
 // Set the corresponding <p> elements in the GetSessionData.html page with the values from above
